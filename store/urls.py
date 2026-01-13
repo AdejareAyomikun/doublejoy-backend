8725d16f250250b45af9b3104dd24b008a3db70d
Adejare Faruk <farukpamilerin@gmail.com>
2026-01-13 08:41:54 +0100

    updated store/urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet
+from .views import ProductViewSet, CategoryViewSet, CartViewSet, OrderViewSet, paystack_webhook
+from store.api.admin_analytics_views import AdminDashboardAnalytics
+from store.api.debug_auth_view import DebugJWTView
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -8,4 +10,9 @@ router.register(r"categories", CategoryViewSet)
 router.register(r"cart", CartViewSet, basename="cart")
 router.register(r"orders", OrderViewSet, basename="orders")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("paystack/webhook/", paystack_webhook),
+    path("debug/jwt/", DebugJWTView.as_view()),
+    path("admin/analytics/", AdminDashboardAnalytics.as_view()),
+]
