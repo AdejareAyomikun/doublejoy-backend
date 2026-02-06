097056d2ab3a40cb15297bc722e31c13880a2a13
Adejare Faruk <farukpamilerin@gmail.com>
2026-02-06 06:02:53 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -131,10 +131,7 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
